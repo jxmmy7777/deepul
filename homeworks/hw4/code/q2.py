bd550a1d7784811e71462ae15097cb7ef21006da
wjchang <weijer_chang@berkeley.edu>
2024-03-15 12:02:55 -0700

    add difusion

--- homeworks/hw4/code/q2.py
+++ homeworks/hw4/code/q2.py
@@ -19,13 +19,20 @@ warnings.filterwarnings('ignore')
 
 from utils import *
 from torch.utils.data import DataLoader, TensorDataset
+
+
+def noise_strength(t):
+    alpha_t = torch.cos(torch.pi / 2 * t)
+    sigma_t = torch.sin(torch.pi / 2 * t)
+    return alpha_t, sigma_t
+
 def timestep_embedding(timesteps, dim, max_period=10000):
     half_dim = dim // 2
     device = timesteps.device  
     freqs = torch.exp(-torch.log(torch.tensor(max_period, device=device, dtype=torch.float32)) * torch.arange(0, half_dim, device=device, dtype=torch.float32) / half_dim)
     
     # Ensure timesteps is a float tensor for multiplication
-    timesteps = timesteps.float().unsqueeze(1)  # Adds an extra dimension at index 1 for broadcasting
+    timesteps = timesteps.float().unsqueeze(-1)
     args = timesteps * freqs
     
     # Calculate sin and cos components
@@ -35,7 +42,7 @@ def timestep_embedding(timesteps, dim, max_period=10000):
         zero_pad = torch.zeros(embedding.shape[0], 1, device=device, dtype=torch.float32)
         embedding = torch.cat([embedding, zero_pad], dim=-1)
     
-    return embedding
+    return embedding #(B,D)
 
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, temb_channels, num_groups=8):
@@ -58,7 +65,7 @@ class ResidualBlock(nn.Module):
             self.proj = nn.Conv2d(in_channels, out_channels, kernel_size=1)
         else:
             self.proj = nn.Identity()
-    def forward(x, temb):
+    def forward(self, x, temb):
         h = self.conv1(x)
         h = self.norm1(h)
         h = F.silu(h)
@@ -89,6 +96,8 @@ class UNet(nn.Module):
         super(UNet, self).__init__()  # Correctly initialize the superclass.
         self.temb_channels = hidden_dims[0] * 4
         
+        self.input_shape = (in_channels, 32, 32)
+        
         self.first_hidden_dim = hidden_dims[0]
         self.time_emb_mlp = nn.Sequential(
             nn.Linear(hidden_dims[0],  self.temb_channels),
@@ -99,38 +108,44 @@ class UNet(nn.Module):
         #blocks
         self.init_conv = nn.Conv2d(in_channels, hidden_dims[0], kernel_size=3, padding=1)
         
-        self.down_blocks = nn.ModuleList()
-        self.down_transitions = nn.ModuleList()
+        self.downs = nn.ModuleList()
         prev_ch = hidden_dims[0]
         down_block_chans = [prev_ch]
-        for i,dim in enumerate(hidden_dims):
-            for _ in range(blocks_per_dim):
-                self.down_blocks.append(ResidualBlock(prev_ch, dim, self.temb_channels))
-                prev_ch = dim
-                down_block_chans.append(prev_ch)
-                
-            if i != len(hidden_dims) - 1:
-                self.down_transitions.append(Downsample(prev_ch))
-                down_block_chans.append(prev_ch)
+        in_out = list(zip(hidden_dims[:-1], hidden_dims[1:]))
         
+        
+        num_resolutions = len(in_out)
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (num_resolutions - 1)
+            
+            self.downs.append(
+                nn.ModuleList([
+                    ResidualBlock(dim_in, dim_out, self.temb_channels),
+                    ResidualBlock(dim_out, dim_out, self.temb_channels),
+                    Downsample(dim_out) if not is_last else nn.Identity()
+                ])
+            )
+        
+        prev_ch = hidden_dims[-1]
         # Middle blocks
-        self.middle_blocks = nn.Sequential(
-            ResidualBlock(prev_ch, prev_ch, self.temb_channels),
-            ResidualBlock(prev_ch, prev_ch, self.temb_channels)
-        )
+        self.middle_block_1 = ResidualBlock(prev_ch, prev_ch, self.temb_channels)
+        self.middle_block_2 = ResidualBlock(prev_ch, prev_ch, self.temb_channels)
         
         # Upsample path
-        self.up_blocks = nn.ModuleList()
-        self.up_transitions = nn.ModuleList()
-        for i, dim in enumerate(reversed(hidden_dims)):
-            for j in range(blocks_per_dim + 1):
-                dch = down_block_chans.pop()
-                self.up_blocks.append(ResidualBlock(prev_ch + dch, dim, self.temb_channels))
-                prev_ch = dim
+        self.ups = nn.ModuleList()
+        
+        for ind, (dim_in, dim_out) in enumerate(reversed(in_out[1:])):
+            is_last = ind >= (num_resolutions - 1)
+            
+            input_dim = (dim_in + dim_out)*2 if ind > 0 else dim_out * 2
+            self.ups.append(nn.ModuleList([
+                ResidualBlock( input_dim, dim_out, self.temb_channels),
+                ResidualBlock(dim_out, dim_out, self.temb_channels),
+                Upsample(dim_out) if not is_last else nn.Identity()
                 
-                if i==blocks_per_dim and j==blocks_per_dim:
-                    self.up_transitions.append(Upsample(prev_ch))
-                
+            ]))
+
+        prev_ch = dim_out
         self.final_norm = nn.GroupNorm(num_groups=8, num_channels=prev_ch)
         self.final_conv = nn.Conv2d(prev_ch, in_channels, kernel_size=3, padding=1)
 
@@ -143,67 +158,34 @@ class UNet(nn.Module):
         hs.append(h)
         
         # Downsampling
-        for block, transition in zip(self.down_blocks, self.down_transitions):
-            h = block(h, temb)
+        for resnet, resnet2, down in self.downs:
+            h = resnet(h, temb)
+            h = resnet2(h, temb)
             hs.append(h)
-            h = transition(h)
-        
+            h = down(h)
         # Middle
-        h = self.middle_blocks(h, temb)
-        
+        h = self.middle_block_1(h, temb)
+        h = self.middle_block_2(h, temb)
         # Upsampling
-        for transition, block in zip(self.up_transitions, self.up_blocks):
-            h = transition(h)
-            skip_connection = hs.pop()
-            h = torch.cat([h, skip_connection], dim=1)
-            h = block(h, temb)
-        
+        #hs [torch.Size([1, 64, 32, 32]), torch.Size([1, 128, 32, 32]), torch.Size([1, 256, 16, 16]), torch.Size([1, 512, 8, 8])]
+        for resnet, resnet2, up in self.ups:
+            h = torch.cat([h, hs.pop()], dim=1) #2*hidden_dim , next hidden_dim+prev_hiddem
+            h = resnet(h, temb)
+            h = resnet2(h, temb)
+            h = up(h)
+            
+            
+       
         h = self.final_norm(h)
         h = F.silu(h)
         out = self.final_conv(h)
-        
         return out
-        # # Given x, t
-        # # temb_channels = hidden_dims[0] * 4
-        # emb = timestep_embedding(t, hidden_dims[0])
-        # emb = self.time_emb_mlp(emb)
-
-        # h = Conv2d(in_channels, hidden_dims[0], 3, padding=1)(x)
-        # hs = [h]
-        # prev_ch = hidden_dims[0]
-        # down_block_chans = [prev_ch]
-        # for i, hidden_dim in enumerate(hidden_dims):
-        #     for _ in range(blocks_per_dim):
-        #         h = ResidualBlock(prev_ch, hidden_dim, temb_channels)(h, emb)
-        #         hs.append(h)
-        #         prev_ch = hidden_dim
-        #         down_block_chans.append(prev_ch)
-        #     if i != len(hidden_dims) - 1:
-        #         h = Downsample(prev_ch)(h)
-        #         hs.append(h)
-        #         down_block_chans.append(prev_ch)
-
-        # h = ResidualBlock(prev_ch, prev_ch, temb_channels)(h, emb)
-        # h = ResidualBlock(prev_ch, prev_ch, temb_channels)(h, emb)
-
-        # for i, hidden_dim in list(enumerate(hidden_dims))[::-1]:
-        #     for j in range(blocks_per_dim + 1):
-        #         dch = down_block_chans.pop()
-        #         h = ResidualBlock(prev_ch + dch, hidden_dim, temb_channels)(cat(h, hs.pop()), emb)
-        #         prev_ch = hidden_dim
-        #         if i and j == blocks_per_dim:
-        #             h = Upsample(prev_ch)(h)
-
-        # h = GroupNorm(num_groups=8, num_channels=prev_ch)(h)
-        # h = SiLU()(h)
-        # out = Conv2d(prev_ch, in_channels, 3, padding=1)(h)
-        # return out
+ 
 
 class ContinuousGaussianDiffusion(nn.Module):
     def __init__(self, model):
         super().__init__()
         self.model = model  # The neural network model f(x,t)
-        self.z_dim = 2
 
     @staticmethod
     def noise_strength(t):
@@ -217,30 +199,41 @@ class ContinuousGaussianDiffusion(nn.Module):
         alpha_t, sigma_t = noise_strength(t)
         alpha_tm1, sigma_tm1 = noise_strength(tm1)
         
+        expand_shape = (-1,) + (1,) * (x.dim() - 1)  # Creates a shape like [-1, 1, 1, 1] for 4D or [-1, 1] for 2D
+        alpha_t = alpha_t.view(expand_shape)
+        sigma_t = sigma_t.view(expand_shape)
+        alpha_tm1 = alpha_tm1.view(expand_shape)
+        sigma_tm1 = sigma_tm1.view(expand_shape)
         #𝜂_t
         noise_scale_t =  sigma_tm1/sigma_t * torch.sqrt(1 - (alpha_t/alpha_tm1)**2)
         x_tm1 = alpha_tm1 * (x - sigma_t * eps_hat)/alpha_t + \
                     torch.sqrt(torch.relu(sigma_tm1**2 -  noise_scale_t**2))*eps_hat +\
                     noise_scale_t*torch.randn_like(x)
+        #clip x_tm1 to -1 to 1
+        x_tm1 = torch.clamp(x_tm1, -1, 1)
         return x_tm1
     @torch.no_grad()
     def ddpm_smapler(self, num_steps, num_samples = 2000, device="cuda"):
         ts = torch.linspace(1 - 1e-4, 1e-4, num_steps + 1, device=device)
-        x =  torch.randn(num_samples, self.z_dim, device=device)
+        x =  torch.randn((num_samples,*self.model.input_shape) , device=device)
       
         for i in range(num_steps):
             t = ts[i]
             tm1 = ts[i + 1]
             #t should be same shape as x
-            t = t.expand(x.shape[0], 1)
-            tm1 = tm1.expand(x.shape[0], 1)
+            t = t.expand(x.shape[0])
+            tm1 = tm1.expand(x.shape[0])
             eps_hat = self.model(x, t)
             x = self.DDPM_UPDATE(x, eps_hat, t, tm1)
         return x
 
     def forward_diffusion(self, x, t):
         alpha_t, sigma_t = noise_strength(t)
-        epsilon = torch.randn_like(x) #uniform sampling
+        epsilon = torch.randn_like(x) #uniform sampling 
+        #alpha_t,sigma_t is shape (B), should expand to same shape as x?
+        if len(x.shape) == 4:
+            alpha_t = alpha_t[:,None,None,None]
+            sigma_t = sigma_t[:,None,None,None]
         x_noised = alpha_t * x + sigma_t * epsilon
         return x_noised, epsilon
 
@@ -249,7 +242,9 @@ class ContinuousGaussianDiffusion(nn.Module):
         return F.mse_loss(epsilon, epsilon_hat, reduction= "mean") 
 
     def loss(self, x):
-        t = torch.rand((*x.shape[:-1], 1), device=x.device) #uniform from 0-1
+        t = torch.rand((x.shape[0]), device=x.device) #uniform from 0-1, but should be same accross batch
+        #if t is not in the same shape as x, need to braodcast
+       
         x_noised, epsilon = self.forward_diffusion(x, t)
         loss = self.loss_function(x_noised, epsilon, t)
         
@@ -269,30 +264,27 @@ def q2(train_data, test_data):
       1 to 512, i.e. np.power(2, np.linspace(0, 9, 10)).astype(int)
     """
     #normalize the data
-    train_args = {'epochs': 100, 'lr': 1e-3, "scheduler":True}
-    mean = np.mean(train_data, axis=0)
-    std = np.std(train_data, axis=0)
+    train_args = {'epochs': 1, 'lr': 1e-3, "scheduler":True}
+   
+    Unet = UNet(in_channels=3)
     
-    mlp = MLP(input_size=3, hidden_size=64, output_size=2)
-    
-    model = ContinuousGaussianDiffusion(mlp)
+    model = ContinuousGaussianDiffusion(Unet)
     #device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
-    train_data = (train_data - mean) / std
-    test_data = (test_data - mean) / std
-
+    train_data = (train_data - 0.5)*2
+    test_data = (test_data - 0.5)*2
     # Convert to PyTorch tensors
-    train_tensor = torch.tensor(train_data, dtype=torch.float32)
-    test_tensor = torch.tensor(test_data, dtype=torch.float32)
+    train_tensor = torch.tensor(train_data, dtype=torch.float32).permute(0, 3, 1, 2)
+    test_tensor = torch.tensor(test_data, dtype=torch.float32).permute(0, 3, 1, 2)
 
     # Create TensorDatasets
     train_dataset = TensorDataset(train_tensor, train_tensor) # Assuming you want to use train_data both as input and target
     test_dataset = TensorDataset(test_tensor, test_tensor) # Same assumption as above
 
     # Create DataLoaders
-    train_loader = DataLoader(train_dataset, batch_size=1024, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=1024)
+    train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=256)
 
     #TODO pass scheduler config to train_args
 
@@ -302,17 +294,17 @@ def q2(train_data, test_data):
     sample_steps = np.power(2, np.linspace(0, 9, 9)).astype(int)
     samples_list = []
     for i in range(len(sample_steps)):
-        samples = model.ddpm_smapler(sample_steps[i])
+        samples = model.ddpm_smapler(sample_steps[i]).permute(0, 2, 3, 1)
         samples_list.append(samples.detach().cpu().numpy())
     all_samples = np.array(samples_list)
     #unormalize samples
-    all_samples = all_samples * std + mean
+    all_samples = all_samples * 0.5 + 0.5
     return train_losses["loss"], test_losses["loss"], all_samples
 
 if __name__ == "__main__":
-    # q2_save_results(q2)
+    q2_save_results(q2)
     #test Unet 
-    model = UNet(3)
-    input = torch.randn(1, 3, 32, 32)
-    t = torch.tensor([10])
-    out = model(input, t)
+    # model = UNet(3)
+    # input = torch.randn(1, 3, 32, 32)
+    # t = torch.tensor([10])
+    # out = model(input, t)
